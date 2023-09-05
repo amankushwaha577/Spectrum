2d4706421e32f5262fe888fc2bf3b42ef84778c6
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-05 22:35:03 +0530

    Jumbrot images

--- ecommerce-front/src/core/Layout.js
+++ ecommerce-front/src/core/Layout.js
@@ -2,6 +2,13 @@ import React from "react";
 import Menu from "./Menu";
 import "../styles.css";
 
+import logo from "../images/logo.png";
+
+const jumbotronStyle = {
+    marginTop:"40px",
+    background:{logo}
+  }
+
 const Layout = ({
     title = "Title",
     description = "Description",
@@ -10,7 +17,7 @@ const Layout = ({
 }) => (
     <div>
         <Menu />
-        <div className="jumbotron" style={{marginTop:"50px"}}>
+        <div className="jumbotron" style={jumbotronStyle}>
             <h2>{title}</h2>
             <p className="lead">{description}</p>
         </div>
